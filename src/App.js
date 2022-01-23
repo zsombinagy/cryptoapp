d9a4142ed60d110b772abdc23b099d9f49d166fa
iamzsombi <zsombinagy.14@gmail.com>
2022-01-23 22:51:37 +0100

    need help

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from "antd";
 
 import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
@@ -9,16 +9,53 @@ import "./App.css"
 
 const App = () => {
   return (
-  <div className='app'>
-      <div className="navbar">
-        <Navbar />
+    <Router>
+      <div className='app'>
+          <div className="navbar">
+            <Navbar />
+          </div>
+          <div className="main">
+            <Layout >
+            <Routes >
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+            </Routes>
+            <Routes >
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+            </Routes>
+            <Routes >
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+            </Routes>
+            <Routes >
+              <Route exact path="/crypto/:coinID">
+                <CryptoDetails />
+              </Route>
+            </Routes>
+            <Routes >
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Routes>
+            </Layout>
+          </div>
+          <div className="footer" >
+            <Typography.Title  level={5} style={{color: "white", textAlign: "center"}}>
+              Cryptoverse <br />
+              ALl rights reserved
+            </Typography.Title>
+            <Space>
+              <Link to="/" >Home</Link>
+              <Link to="/exchanges" >Exchanges</Link>
+              <Link to="/news" >News</Link>
+            </Space>
+          </div>
       </div>
-      <div className="main">
-      </div>
-      <div className="footer">
-
-      </div>
-  </div>
+    </Router>
 )
 };
 
